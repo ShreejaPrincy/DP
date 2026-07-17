8d287e4aa538803b772e9ade425afccdec08641f
Shreeja Princy <148578262+ShreejaPrincy@users.noreply.github.com>
2026-07-17 22:17:47 +0530

    Update 213. House Robber II.cpp

--- 213. House Robber II.cpp
+++ 213. House Robber II.cpp
@@ -120,3 +120,57 @@ public:
         return max(withFirst, withoutFirst);
     }
 };
+
+
+//--------------------------------------------------------------------
+//striver's code
+// Space Optimization
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+// (O(n) for temp1 and temp2, O(1) extra space in the helper function)
+
+class Solution {
+public:
+    int solve(vector<int>& nums) {
+
+        int prev1 = nums[0];
+        int prev2 = 0;
+
+        for (int i = 1; i < nums.size(); i++) {
+
+            int take = nums[i];
+            if (i > 1)
+                take += prev2;
+
+            int skip = prev1;
+
+            int curr = max(take, skip);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
+    int rob(vector<int>& nums) {
+
+        int n = nums.size();
+
+        if (n == 1)
+            return nums[0];
+
+        vector<int> temp1, temp2;
+
+        for (int i = 0; i < n; i++) {
+
+            if (i != n - 1)
+                temp1.push_back(nums[i]);
+
+            if (i != 0)
+                temp2.push_back(nums[i]);
+        }
+
+        return max(solve(temp1), solve(temp2));
+    }
+};

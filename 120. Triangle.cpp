f3a90b5ecbc126660f1dbeb99aad15312307c436
Shreeja Princy <148578262+ShreejaPrincy@users.noreply.github.com>
2026-08-08 19:31:15 +0530

    Update 120. Triangle.cpp

--- 120. Triangle.cpp
+++ 120. Triangle.cpp
@@ -102,3 +102,30 @@ public:
         return dp[0][0];
     }
 };
+
+
+
+// Space Optimization
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+
+        int n = triangle.size();
+
+        vector<int> dp = triangle[n - 1];
+
+        for(int i = n - 2; i >= 0; i--) {
+
+            for(int j = 0; j <= i; j++) {
+
+                dp[j] = triangle[i][j] +
+                        min(dp[j], dp[j + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+};

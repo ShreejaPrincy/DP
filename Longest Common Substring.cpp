c979b8920fc80b2915718cd92d091fdad2a63657
Shreeja Princy <148578262+ShreejaPrincy@users.noreply.github.com>
2026-08-14 17:15:54 +0530

    Update Longest Common Substring.cpp

--- Longest Common Substring.cpp
+++ Longest Common Substring.cpp
@@ -79,3 +79,49 @@ public:
         return result;
     }
 };
+
+
+
+
+
+//Space Optimisation
+// TC: O(n*m)
+// SC: O(m)
+
+// NOTE: Index Shifting
+// We use i = 1 to n and j = 1 to m.
+// Therefore, actual string indices are:
+// s1[i-1] and s2[j-1].
+// Extra 0th row/column represent an empty string.
+
+class Solution {
+public:
+    int longCommSubstr(string& s1, string& s2) {
+
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<int> prev(m + 1, 0);
+        vector<int> curr(m + 1, 0);
+
+        int result = 0;
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = 1; j <= m; j++) {
+
+                if(s1[i-1] == s2[j-1])
+                    curr[j] = 1 + prev[j-1];
+
+                else
+                    curr[j] = 0;
+
+                result = max(result, curr[j]);
+            }
+
+            prev = curr;
+        }
+
+        return result;
+    }
+};

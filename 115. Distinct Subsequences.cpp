adfae1355505b5e86a39da7145a4811363304c6d
Shreeja Princy <148578262+ShreejaPrincy@users.noreply.github.com>
2026-08-18 17:07:23 +0530

    Update 115. Distinct Subsequences.cpp

--- 115. Distinct Subsequences.cpp
+++ 115. Distinct Subsequences.cpp
@@ -1,5 +1,6 @@
 //Recursion
-
+// TC: O(2^n)
+// SC: O(n) -> Recursion stack
 class Solution {
 public:
     int solve(int i,int j,string &s,string &t){
@@ -21,7 +22,8 @@ public:
 
 
 //Memoisation
-
+// TC: O(n*m)
+// SC: O(n*m) + O(n) -> DP table + Recursion stack
 class Solution {
 public:
     int solve(vector<vector<int>>&dp,int i,int j,string &s,string &t){
@@ -45,3 +47,57 @@ public:
 };
 
 
+//Tabulation
+// TC: O(n*m)
+// SC: O(n*m)
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+
+        vector<vector<unsigned int>>dp(n+1,vector<unsigned int>(m+1,0));
+
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+
+                if(s[i-1]==t[j-1])  dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+
+                else                dp[i][j]=dp[i-1][j];
+            }
+        }
+
+        return (int)dp[n][m];
+    }
+};
+
+
+
+//space optimisation in 1D
+// TC: O(n*m)
+// SC: O(m)
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+
+        vector<double>prev(m+1,0);
+
+        prev[0]=1;        
+
+        for(int i=1;i<=n;i++){
+            for(int j=m;j>=1;j--){
+
+                if(s[i-1]==t[j-1])  prev[j]=prev[j-1]+prev[j];
+
+            }
+        }
+
+        return (int)prev[m];
+    }
+};

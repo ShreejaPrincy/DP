ff4f9c7c25b3cef4b71a3422707e5c5b81b70644
Shreeja Princy <148578262+ShreejaPrincy@users.noreply.github.com>
2026-08-21 10:42:00 +0530

    Add 1D DP solution for longest increasing subsequence
    Implemented a 1D dynamic programming solution for finding the length of the longest increasing subsequence with O(n^2) time complexity and O(n) space complexity.

--- 300. Longest Increasing Subsequence.cpp
+++ 300. Longest Increasing Subsequence.cpp
@@ -117,3 +117,36 @@ public:
         return front[0];
     }
 };
+
+
+
+
+// 1D DP(another method)
+// TC: O(n^2)
+// SC: O(n)
+
+class Solution {
+public:
+    int n;
+
+    int lengthOfLIS(vector<int>& nums) {
+        n=nums.size();
+
+        vector<int>dp(n+1,1);
+
+        int ans=1;
+
+        for(int i=1;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+
+                if(nums[prev]<nums[i]){
+                    dp[i]=max(dp[i],1+dp[prev]);
+                }
+            }
+
+            ans=max(ans,dp[i]);
+        }
+
+        return ans;
+    }
+};
